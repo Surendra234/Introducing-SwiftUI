64999bf7386c694251c32c2b12e3bfc6df9ba924
MrSurendraMahawar <101715947+Surendra234@users.noreply.github.com>
2023-11-21 11:29:17 +0530

    first commit

---
+++ Introducing-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Introducing-SwiftUI
+//
+//  Created by XP India on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Introducing-SwiftUI/Introducing_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Introducing_SwiftUIApp.swift
+//  Introducing-SwiftUI
+//
+//  Created by XP India on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Introducing_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
